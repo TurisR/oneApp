35c79bcc3f57208b6cdd53809e5198e3ad67e048
yxy98Git <961240991@qq.com>
2019-11-15 10:53:04 +0800

    添加 播放界面 与 相关的Activity

---
+++ app/src/main/java/com/example/music_app/View/Activity/PlayViewActivity.java
@@ -0,0 +1,27 @@
+package com.example.music_app.View.Activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.example.music_app.R;
+
+/**
+ *  內容：播放界面Activity
+ *  编写：yxy
+ *  版本: 1.0
+ *  时间：2019/11/15
+ */
+
+public class PlayViewActivity extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play_view);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
